d6f39adc59a6eef3870eb04bba95a3cf28e6185a
Asief Mahir <asiefmahir1@gmail.com>
2026-01-27 23:48:40 +0600

    feat: Setup Firebase Firestore and Client State management with Redux Toolkit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCwLEcHfcsx4dDy7VYqjVzv7vPzfaCa6Do",
+	authDomain: "react-simple-project-36f8f.firebaseapp.com",
+	projectId: "react-simple-project-36f8f",
+	storageBucket: "react-simple-project-36f8f.firebasestorage.app",
+	messagingSenderId: "156134273705",
+	appId: "1:156134273705:web:aac1f468f053b47e862fb8",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { ourRouter } from "./router/router.jsx";
+// import App from "./App.jsx";
 import { RouterProvider } from "react-router";
+import { rootRouter } from "./router/router.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={ourRouter} />
+		<Provider store={store}>
+			<RouterProvider router={rootRouter} />
+		</Provider>
 	</StrictMode>,
 );

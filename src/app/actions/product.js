cfa3d6dcb9e1c921994768a7b1215d2d1c6e62ed
Asief Mahir <asiefmahir1@gmail.com>
2026-02-28 00:01:26 +0600

    fix: fix the product post req url for production

--- src/app/actions/product.js
+++ src/app/actions/product.js
@@ -3,10 +3,12 @@
 //ssg -> isr -> time based isr
 // on demand isr -> action based -> event based
 import { updateTag } from "next/cache";
+import { getBaseUrl } from "../utils/api";
 
 // server action
 export const addProduct = async (product) => {
-	await fetch(`http://localhost:3000/api/product`, {
+	const baseUrl = getBaseUrl();
+	await fetch(`${baseUrl}/api/product`, {
 		method: "POST",
 		body: JSON.stringify(product),
 		headers: {
